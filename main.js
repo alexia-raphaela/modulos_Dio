d6bc40e3f0c803c67005e86e6a41a17a327b13d8
Alexia <alexiaraphaela@gmail.com>
2022-06-01 15:52:41 -0300

    demonstra exportação e importação de um arquivo.

---
+++ main.js
@@ -0,0 +1,9 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs'
+
+function main(){
+    console.log(mostraIdade('alexia', 21))
+    console.log(mostraCidade('alexia', 'peruibe'))
+    console.log(mostraHobby('alexia', 'dancar'))
+}
+
+main()
